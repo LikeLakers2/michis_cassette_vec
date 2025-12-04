1966ceac5d4c9c160c687bf4a476c31d3a6c6991
MichiRecRoom <1008889+LikeLakers2@users.noreply.github.com>
2025-12-03 21:32:41 -0500

    refactor around the TapeLike trait, since that's the best option i have

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
-extern crate alloc;
-
-use alloc::collections::VecDeque;
+mod tapelike_impls;
 
 #[derive(Clone, Debug, Default, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub struct CassetteVec<Tape> {
@@ -27,7 +25,7 @@ impl<Tape> CassetteVec<Tape> {
 	///
 	/// This can be assumed to uphold `0 <= head_position <= tape_len`, where `head_position` is the
 	/// value returned by this function, and `tape_len` is the value returned by
-	/// `self.as_tape().len()`.
+	/// `self.tape_len()`.
 	pub fn head_position(&self) -> usize {
 		self.head
 	}
@@ -40,10 +38,10 @@ impl<Tape> CassetteVec<Tape> {
 	/// Gets a mutable reference to the underlying tape.
 	///
 	/// If the underlying tape's length is modified, you **must** ensure that the cassette head
-	/// position remains within the bounds of the tape. Failure to do is a logic error. The behavior
-	/// resulting from such a logic error is not specified, but will be encapsulated to the
-	/// `CassetteVec` that observed the logic error and not result in undefined behavior. This could
-	/// include panics, incorrect results, and other such unwanted behavior.
+	/// position is within the bounds of the tape before the next read/write. Failure to do is a
+	/// logic error. The behavior resulting from such a logic error is not specified, but will be
+	/// encapsulated to the `CassetteVec` that observed the logic error and not result in undefined
+	/// behavior. This could include panics, incorrect results, and other such unwanted behavior.
 	pub fn get_mut(&mut self) -> &mut Tape {
 		&mut self.tape
 	}
@@ -53,39 +51,42 @@ impl<Tape> CassetteVec<Tape> {
 	}
 }
 
+// Head operations
 impl<Tape: TapeLike> CassetteVec<Tape> {
-	/// # Panics
-	/// Panics if an attempt is made to seek before `usize::MIN`, or seek past the `len()` of the
-	/// vec.
-	// TODO: Make panics into a Result
-	pub fn seek(&mut self, pos: SeekFrom) -> usize {
+	// TODO: Change to something like `Result<usize, OutOfBoundsError>`
+	pub fn seek(&mut self, pos: SeekFrom) -> Option<usize> {
 		let tape_len = self.tape.len();
 
-		let new_head = match pos {
-			SeekFrom::Start(p) => p,
-			SeekFrom::End(p) => tape_len.checked_sub_signed(p).unwrap(),
-			SeekFrom::Current(p) => self.head.checked_add_signed(p).unwrap(),
+		let desired_position = match pos {
+			SeekFrom::Start(p) => Some(p),
+			SeekFrom::End(p) => tape_len.checked_add_signed(p),
+			SeekFrom::Current(p) => self.head.checked_add_signed(p),
 		};
 
-		if new_head <= tape_len {
-			self.head = new_head;
-		} else {
-			panic!();
-		}
-
-		self.head
+		desired_position
+			.filter(|&pos| pos <= tape_len)
+			.inspect(|&new_pos| self.head = new_pos)
 	}
 
-	pub fn rewind(&mut self) {
+	pub fn seek_to_start(&mut self) {
 		self.head = 0;
 	}
 
-	/// # Panics
-	/// Panics if an attempt is made to seek before `usize::MIN`, or seek past the `len()` of the
-	/// vec.
-	// TODO: Make panics into a Result
-	pub fn seek_relative(&mut self, offset: isize) -> usize {
+	pub fn move_backwards(&mut self) -> bool {
+		self.seek_relative(-1).is_some()
+	}
+
+	// TODO: Change to something like `Result<usize, OutOfBoundsError>`
+	pub fn seek_relative(&mut self, offset: isize) -> Option<usize> {
 		self.seek(SeekFrom::Current(offset))
+	}
+
+	pub fn move_forward(&mut self) -> bool {
+		self.seek_relative(1).is_some()
+	}
+
+	pub fn seek_to_last_item(&mut self) {
+		self.head = self.tape.len().checked_sub(1).unwrap_or_default();
 	}
 
 	pub fn seek_to_end(&mut self) {
@@ -93,13 +94,8 @@ impl<Tape: TapeLike> CassetteVec<Tape> {
 	}
 }
 
-// Tape operations
+// Tape ref operations
 impl<Tape: TapeLike> CassetteVec<Tape> {
-	pub fn clear(&mut self) {
-		self.tape.clear();
-		self.head = 0;
-	}
-
 	pub fn tape_len(&self) -> usize {
 		self.tape.len()
 	}
@@ -107,13 +103,21 @@ impl<Tape: TapeLike> CassetteVec<Tape> {
 	pub fn get_item_at_head(&self) -> Option<&Tape::Item> {
 		self.tape.get_item(self.head)
 	}
+}
+
+// Tape mut operations
+impl<Tape: TapeLikeMut> CassetteVec<Tape> {
+	pub fn clear(&mut self) {
+		self.tape.clear();
+		self.head = 0;
+	}
 
 	pub fn get_item_at_head_mut(&mut self) -> Option<&mut Tape::Item> {
 		self.tape.get_item_mut(self.head)
 	}
 
 	pub fn set_item_at_head(&mut self, item: Tape::Item) {
-		self.tape.set_item_at(self.head, item);
+		self.tape.set_item(self.head, item);
 	}
 
 	// TODO: Should this be added back the same, or should it be replaced by a "truncate" function?
@@ -129,8 +133,30 @@ impl<Tape: TapeLike> CassetteVec<Tape> {
 
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub enum SeekFrom {
+	/// Moves the cassette head to the provided index.
+	///
+	/// # Examples
+	/// * `SeekFrom::Start(0)` will move the cursor to the first item
+	/// * `SeekFrom::Start(5)` will move the cursor to the sixth item
 	Start(usize),
+	/// Moves the cassette head to the cassette's length (as provided by [`CassetteVec::tape_len`])
+	/// plus the provided number of indices.
+	///
+	/// It is an error to seek before the first index, or more than one index past the last item.
+	///
+	/// # Examples
+	/// * `SeekFrom::End(-1)` will move the cursor to the last item, if one exists
+	/// * `SeekFrom::End(0)` will move the cursor to the index just after the last item
 	End(isize),
+	/// Moves the cassette head to the current position (as provided by
+	/// [`CassetteVec::head_position`]) plus the provided number of indices.
+	///
+	/// It is an error to seek before the first index, or more than one index past the last item.
+	///
+	/// # Examples
+	/// * `SeekFrom::Current(-2)` will move the cursor back two indices
+	/// * `SeekFrom::Current(0)` won't move anywhere
+	/// * `SeekFrom::Current(5)` will move the cursor forward five indices
 	Current(isize),
 }
 
@@ -143,55 +169,10 @@ pub trait TapeLike {
 
 	fn len(&self) -> usize;
 	fn get_item(&self, index: usize) -> Option<&Self::Item>;
+}
+
+pub trait TapeLikeMut: TapeLike {
 	fn get_item_mut(&mut self, index: usize) -> Option<&mut Self::Item>;
-	fn set_item_at(&mut self, index: usize, item: Self::Item);
+	fn set_item(&mut self, index: usize, item: Self::Item);
 	fn clear(&mut self);
-}
-
-impl<T> TapeLike for Vec<T> {
-	type Item = T;
-
-	fn len(&self) -> usize {
-		self.len()
-	}
-
-	fn get_item(&self, index: usize) -> Option<&Self::Item> {
-		self.get(index)
-	}
-
-	fn get_item_mut(&mut self, index: usize) -> Option<&mut Self::Item> {
-		self.get_mut(index)
-	}
-
-	fn set_item_at(&mut self, index: usize, item: Self::Item) {
-		self.insert(index, item);
-	}
-
-	fn clear(&mut self) {
-		self.clear();
-	}
-}
-
-impl<T> TapeLike for VecDeque<T> {
-	type Item = T;
-
-	fn len(&self) -> usize {
-		self.len()
-	}
-
-	fn get_item(&self, index: usize) -> Option<&Self::Item> {
-		self.get(index)
-	}
-
-	fn get_item_mut(&mut self, index: usize) -> Option<&mut Self::Item> {
-		self.get_mut(index)
-	}
-
-	fn set_item_at(&mut self, index: usize, item: Self::Item) {
-		self.insert(index, item);
-	}
-
-	fn clear(&mut self) {
-		self.clear();
-	}
 }
---
+++ src/tapelike_impls.rs
@@ -0,0 +1,57 @@
+extern crate alloc;
+
+use alloc::collections::VecDeque;
+
+use crate::{TapeLike, TapeLikeMut};
+
+impl<T> TapeLike for Vec<T> {
+	type Item = T;
+
+	fn len(&self) -> usize {
+		self.len()
+	}
+
+	fn get_item(&self, index: usize) -> Option<&Self::Item> {
+		self.get(index)
+	}
+}
+
+impl<T> TapeLikeMut for Vec<T> {
+	fn get_item_mut(&mut self, index: usize) -> Option<&mut Self::Item> {
+		self.get_mut(index)
+	}
+
+	fn set_item(&mut self, index: usize, item: Self::Item) {
+		self.insert(index, item);
+	}
+
+	fn clear(&mut self) {
+		self.clear();
+	}
+}
+
+impl<T> TapeLike for VecDeque<T> {
+	type Item = T;
+
+	fn len(&self) -> usize {
+		self.len()
+	}
+
+	fn get_item(&self, index: usize) -> Option<&Self::Item> {
+		self.get(index)
+	}
+}
+
+impl<T> TapeLikeMut for VecDeque<T> {
+	fn get_item_mut(&mut self, index: usize) -> Option<&mut Self::Item> {
+		self.get_mut(index)
+	}
+
+	fn set_item(&mut self, index: usize, item: Self::Item) {
+		self.insert(index, item);
+	}
+
+	fn clear(&mut self) {
+		self.clear();
+	}
+}
